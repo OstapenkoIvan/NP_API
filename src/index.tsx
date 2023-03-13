f2e86c04d0b848ca5ab0b3fef4528d723a6ddfd0
OstapenkoIvan <ostapenko.ivan@gmail.com>
2023-03-13 13:38:42 +0200

    feat: redux basics

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+import { store } from "./store";
 import "./index.css";
 import App from "./components/App/App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
+let persistor = persistStore(store);
+
+createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
