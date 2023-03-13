a6cf6979b15eb9d4bde0be3243251c3190f7fd2b
OstapenkoIvan <ostapenko.ivan@gmail.com>
2023-03-13 13:38:00 +0200

    feat: redux basics

---
+++ src/store/index.ts
@@ -0,0 +1,24 @@
+import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+// import { newsReducer } from "./news";
+
+export const store = configureStore({
+  reducer: {
+    // news: newsReducer,
+  },
+  devTools: process.env.NODE_ENV === "development",
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
