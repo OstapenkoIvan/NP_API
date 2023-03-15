2c31be30018830124ceb126116ff7bdae8d58385
OstapenkoIvan <ostapenko.ivan@gmail.com>
2023-03-15 15:39:43 +0200

    bugfix: moved index for gh-pages

--- index.tsx
+++ index.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { store, persistedStore } from "./store";
+import { store, persistedStore } from "./src/store";
 import "./index.css";
-import App from "./components/App/App";
+import App from "./src/components/App/App";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
